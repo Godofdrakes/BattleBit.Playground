74e5d6bcd1672f5058b38cf4fb2da76cdd67163c
Godofdrakes <GodofDrakes@gmail.com>
2023-08-31 18:31:49 -0700

    Added module to unlock all attachments

---
+++ Playground.AllWeaponsUnlocked/AllWeaponsUnlockedModule.cs
@@ -0,0 +1,12 @@
+﻿using BBRAPIModules;
+
+namespace Playground.AllWeaponsUnlocked;
+
+public class AllWeaponsUnlockedModule : BattleBitModule
+{
+	public override Task OnConnected()
+	{
+		Server.ServerSettings.UnlockAllAttachments = true;
+		return Task.CompletedTask;
+	}
+}
