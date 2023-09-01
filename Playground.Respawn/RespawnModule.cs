f8bbddcf583ac5209ac29bca8472d7754bfe0da7
Godofdrakes <GodofDrakes@gmail.com>
2023-08-31 19:16:55 -0700

    Fixed module reference

--- Playground.Respawn/RespawnModule.cs
+++ Playground.Respawn/RespawnModule.cs
@@ -6,6 +6,13 @@ namespace Playground.Respawn;
 [RequireModule(typeof(CommandHandler))]
 public class RespawnModule : BattleBitModule
 {
+	public CommandHandler? CommandHandler { get; set; }
+
+	public override void OnModulesLoaded()
+	{
+		CommandHandler?.Register(this);
+	}
+
 	[CommandCallback("respawn")]
 	public void RespawnCommand(RunnerPlayer player)
 	{
