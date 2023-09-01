f5d3ecda5ca7ca4567afb60bb2c23e71a07af9c5
Godofdrakes <GodofDrakes@gmail.com>
2023-09-01 00:31:17 -0700

    MOTD module

---
+++ Playground.MOTD/MOTDModule.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using BBRAPIModules;
+
+namespace Playground.MOTD;
+
+public class MOTDConfig : ModuleConfiguration
+{
+	public string Message { get; set; } = string.Empty;
+}
+
+public class MOTDModule : BattleBitModule
+{
+	public MOTDConfig Config { get; set; } = new();
+
+	public override Task OnPlayerConnected(RunnerPlayer player)
+	{
+		if (!string.IsNullOrEmpty(Config.Message))
+		{
+			player.Message(Config.Message);
+		}
+
+		return Task.CompletedTask;
+	}
+}
